ced4985c1b7cac1ee52a3fdaabfa194bd00baf79
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2022-10-24 19:34:34 +0100

    View Model Implemented
    - Import dependencie
    - Created functions necesssaries to work

--- app/src/main/java/com/money2/ui/viewModel/MoneyViewModel.kt
+++ app/src/main/java/com/money2/ui/viewModel/MoneyViewModel.kt
@@ -1,4 +1,21 @@
 package com.money2.ui.viewModel
 
-class MoneyViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.money2.respository.MoneyRepository
+import kotlinx.coroutines.launch
+
+class MoneyViewModel: ViewModel() {
+
+    private val repository = MoneyRepository()
+
+    fun convertMoney(contry_to: String, country_from: String, money: Double) {
+        viewModelScope.launch {
+
+        }
+    }
+
+    fun getConvertMoney() {
+
+    }
 }
